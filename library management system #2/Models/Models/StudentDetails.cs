ee2455167715c101348e4647a79203f55bf019c6
Locobuzz-TarushGupta <121843592+Locobuzz-TarushGupta@users.noreply.github.com>
2023-01-18 20:51:47 +0530

    Add files via upload

---
+++ library management system #2/Models/Models/StudentDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace library_management_system.Models
+{
+    public class StudentDetails
+    {
+        public string? StudentName { get; set; }
+        public int StudentId { get; set; }
+        public string? StudentAddress { get; set; }
+        public string? StudentEmail { get; set; }
+        public string? StudentPhone { get; set; }
+        public float PenaltyDue { get; set; }
+        public float RentDue { get; set; }
+    }
+}
